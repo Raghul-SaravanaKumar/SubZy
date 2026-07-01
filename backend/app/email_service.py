9c756801ff142cc5097c6de918bbfb3c98349cfe
Raghul18 <rexizzz1815@gmail.com>
2026-07-01 17:20:48 +0530

    Final Changes

--- backend/app/email_service.py
+++ backend/app/email_service.py
@@ -1,22 +1,64 @@
-from pymongo import MongoClient
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+import sib_api_v3_sdk
+from sib_api_v3_sdk.rest import ApiException
 
-# Load environment variables
 load_dotenv()
 
-# MongoDB URI
-MONGO_URI = os.getenv("MONGO_URI")
-
-if not MONGO_URI:
-    raise ValueError("MONGO_URI not found in .env file")
-
-# MongoDB Connection
-client = MongoClient(MONGO_URI)
-
-# Database Name
-db = client["SubZy"]
+BREVO_API_KEY = os.getenv("BREVO_API_KEY")
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
 
 
-def get_db():
-    return db
+def send_email(to_email: str, subject: str, body: str):
+
+    configuration = sib_api_v3_sdk.Configuration()
+    configuration.api_key["api-key"] = BREVO_API_KEY
+
+    api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
+        sib_api_v3_sdk.ApiClient(configuration)
+    )
+
+    email = sib_api_v3_sdk.SendSmtpEmail(
+
+        sender={
+            "name": "SubZy",
+            "email": SENDER_EMAIL
+        },
+
+        to=[
+            {
+                "email": to_email
+            }
+        ],
+
+        subject=subject,
+
+        html_content=f"""
+        <html>
+        <body>
+
+        <h2>SubZy Subscription Reminder</h2>
+
+        <p>{body}</p>
+
+        <br>
+
+        <p>Regards,</p>
+        <b>SubZy Team</b>
+
+        </body>
+        </html>
+        """
+    )
+
+    try:
+
+        response = api_instance.send_transac_email(email)
+
+        return response
+
+    except ApiException as e:
+
+        print(e)
+
+        raise Exception("Unable to send email")
